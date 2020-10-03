e48eb3d2d0a9f657453d5ffc8f9309cf2cccb5bf
Sonja E. Hennessy <35819406+SHenn39910@users.noreply.github.com>
2020-10-02 21:52:05 -0400

    added controller and postman

---
+++ routes/auth.route.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+
+// Load Controllers
+const {
+    registerController
+} = require('../controllers/auth.controller.js')
+
+router.post('/register', registerController)
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
-const cars = require('cars')
+const cors = require('cors')
 
 // Config .env to ./config/config.env
 require('dotenv').config({
@@ -9,10 +9,33 @@ require('dotenv').config({
 })
 const app = express()
 
+// Config for only development
+if(process.env.NODE_ENV === 'development'){
+    app.use(cors({
+        origin: process.env.CLIENT_URL
+    }))
+
+    app.use(morgan('dev'))
+    //Morgan give information about each request
+    //Cors it's allow to deal with react for localhost at port 3000 without any problem
+} 
 
 
+//Load all routes
+
+const authRouter = require('./routes/auth.route')
+
+//Use Routes
+app.use('/api/', authRouter);
+
+app.use((req, res, next) => {
+    res.status(404).json({
+        success: false,
+        message: "Page not found"
+    })
+});
 const PORT = process.env.PORT
 
 app.listen(PORT, () => {
-    console.log('App listening on port ${PORT}');
+    console.log(`App listening on port ${PORT}`);
 });
